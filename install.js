500e305f1b3337aa0e8fa095b9846e8da63c4f1a
parabirb <parabirb@protonmail.ch>
2021-09-22 22:34:14 -0500

    fix mistake in installer

--- install.js
+++ install.js
@@ -5,7 +5,7 @@
 (async () => {
     $log("<b>Welcome to the sus93 installer</b>");
     $log("sus93 is a package manager for Windows 93.");
-    $log("Adding sus93 to boot file...");
+    $log("Downloading boot file...");
     const settings = { method: "GET", mode: "cors", cache: "reload" };
     localStorage["boot/sus93.js"] = await fetch("https://raw.githubusercontent.com/parabirb/sus93/main/bootscript.js", settings).then(response => response.text());
     $log("Writing to apps file...");
